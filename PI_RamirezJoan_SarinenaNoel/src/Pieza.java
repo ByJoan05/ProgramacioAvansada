797e59a7f567b6fcbe015eeed279bb49395150a7
xNoelSV <noelsava25@gmail.com>
2024-09-29 13:42:39 +0200

    Push

--- PI_RamirezJoan_SarinenaNoel/src/Pieza.java
+++ PI_RamirezJoan_SarinenaNoel/src/Pieza.java
@@ -1,9 +1,71 @@
 public class Pieza {
-    private char PEON;
-    private char CABALLO;
-    private char ALFIL;
-    private char TORRE;
-    private char REINA;
-    private char REY;
+
+    // ATRIBUTOS
+    private final char PEON = 'P';
+    private final char CABALLO = 'C';
+    private final char ALFIL = 'A';
+    private final char TORRE = 'T';
+    private final char REINA = 'Q';
+    private final char REY = 'K';
+    private char columna;
+    private int fila;
+    private char tipo;
+
+    // CONSTRUCTOR
+    public Pieza(char columna, int fila, char tipo) {
+        this.columna = columna;
+        this.fila = fila;
+        this.tipo = tipo;
+    }
+
+    // GETTERS Y SETTERS
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setPosicion(int fila, int columna) {
+        this.fila = fila;
+        // TODO: Transformar columna de char a int
+        // --> this.columna = columna;
+    }
+
+    // TOSTRING
+    @Override
+    public String toString() {
+        return "Pieza{" +
+                "columna=" + columna +
+                ", fila=" + fila +
+                ", tipo=" + tipo +
+                '}';
+    }
+
+    // MÉTODOS
+    public boolean fiJoc () {
+        // TODO: Comprobar si finaliza el juego. Es decir, pos de pieza eliminada = rey
+        return false;
+    }
+
+    public void checkTipo (char tipo) {
+        String tipoCompleto = "";
+        switch (tipo) {
+            case 'P': tipoCompleto = "Peón";
+            case 'C': tipoCompleto = "Caballo";
+            case 'A': tipoCompleto = "Alfil";
+            case 'T': tipoCompleto = "Torre";
+            case 'Q': tipoCompleto = "Reyna";
+            case 'K': tipoCompleto = "Rey";
+            default: throw new IllegalArgumentException();
+        }
+        System.out.println("La pieza es de tipo: " + tipoCompleto);
+    }
 
 }
--- PI_RamirezJoan_SarinenaNoel/src/Torns.java
+++ PI_RamirezJoan_SarinenaNoel/src/Torns.java
@@ -5,24 +5,38 @@ public class Torns <E> {
     private ArrayList<E> llistatTorns;
 
     // CONSTRUCTORS
+
+    // Constructor per defecte que inicialitza el llistat de torns
     public Torns() {
         llistatTorns = new ArrayList<E>();
     }
+    // Invoca el mètode privat llençarà una excepció si no s’ha pogut llegir o si la llista és buida
     public Torns(String nomFitxer) {
         llistatTorns = new ArrayList<E>();
     }
 
     // MÈTODES
+
+    // Afegeix un torn a l’última posició del llistat.
     public void afegirTorn(E torn) {
         llistatTorns.add(torn);
     }
 
-    public E agafarTorn() {
+    // Elimina i retorna el primer element del llistat (que serà el torn a realitzar),
+        // si no hi ha més retornarà una excepció del tipus NoSuchElementException.
+    public E agafarPrimerTorn() {
         return llistatTorns.get(0);
     }
 
+    // Guarda tots els torns de la partida en un fitxer del tipus txt, podria retornar una excepció del tipus IOException.
     public void guardarAFitxer (String nomFitxer) {
         //TODO
+
+    }
+
+    // Mètode privat que llegeix un fitxer txt i inicialitzar el llistat de torns a partir del fitxer.
+    public void carregarDesdeFitxer (String nomFitxer) {
+
 
     }
 
